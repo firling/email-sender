47c2c74eb63305559c9a4bb4a261d18967163f5b
Julien Anquetil <anquetil.jul@gmail.com>
2021-06-28 19:03:28 +0200

    https

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const https = require("https");
+const fs = require("fs");
 require('dotenv').config()
 const cors = require("cors");
+
+const privateKey  = fs.readFileSync('/etc/letsencrypt/live/anquetil.org/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/anquetil.org/fullchain.pem', 'utf8');
+const credentials = {key: privateKey, cert: certificate};
 
 const app = express();
 const port = process.env.PORT || 3333;
@@ -13,6 +19,8 @@ const mainRouter = require('./routes/mainRouter');
 
 app.use('/api', mainRouter);
 
-const server = app.listen(port, () => {
-    console.log(`Web server is running on port ${port}`);
+const serverSSL = https.createServer(credentials, app);
+
+serverSSL.listen(port, () => {
+  console.log(`Web server is running on port ${port}`);
 })
