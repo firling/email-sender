07cab5b8893ef3bea8aa364533bd14e30203c1d4
Julien Anquetil <anquetil.jul@gmail.com>
2021-08-01 14:23:04 +0200

    remove https server (proxy apache)

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express = require("express");
-const https = require("https");
-const fs = require("fs");
 require('dotenv').config()
 const cors = require("cors");
 
-const privateKey  = fs.readFileSync('/etc/letsencrypt/live/anquetil.org/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/anquetil.org/fullchain.pem', 'utf8');
-const credentials = {key: privateKey, cert: certificate};
+// const privateKey  = fs.readFileSync('/etc/letsencrypt/live/anquetil.org/privkey.pem', 'utf8');
+// const certificate = fs.readFileSync('/etc/letsencrypt/live/anquetil.org/fullchain.pem', 'utf8');
+// const credentials = {key: privateKey, cert: certificate};
 
 const app = express();
 const port = process.env.PORT || 3333;
@@ -19,8 +17,12 @@ const mainRouter = require('./routes/mainRouter');
 
 app.use('/api', mainRouter);
 
-const serverSSL = https.createServer(credentials, app);
-
-serverSSL.listen(port, () => {
+app.listen(port, () => {
   console.log(`Web server is running on port ${port}`);
 })
+
+// const serverSSL = https.createServer(credentials, app);
+
+// serverSSL.listen(port, () => {
+//   console.log(`Web server is running on port ${port}`);
+// })
